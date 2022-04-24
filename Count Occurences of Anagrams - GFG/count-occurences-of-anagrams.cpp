9350bbf452751fdb80cfb403a7ceb39144e13995
jitender1222 <93830004+jitender1222@users.noreply.github.com>
2022-04-24 21:19:52 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
@@ -11,40 +11,36 @@ public:
 	int search(string pat, string txt) {
 	    // code here
 	    int ans=0;
-	    unordered_map<char, int>um;
-	    // put all the element into the map ;
-	    for(int i=0;i<pat.size();i++){
+	    unordered_map<char,int>um;
+	    for(int i=0;i<pat.length();i++){
 	        um[pat[i]]++;
 	    }
-	    int k=pat.size();
-	    int i=0,j=0;
-	    int count=um.size();
+	    int i=0,j=0,count=um.size(),k=pat.length();
 	    
-	    while(j<txt.size()){
+	    while(j<txt.length()){
 	        
-	// if my j is present then i have to decrease the character count
-	   if(um.find(txt[j])!=um.end()){
+	        if(um.find(txt[j])!=um.end()){
 	            um[txt[j]]--;
-	  // if all the elemtn present in thr map equals to zro then count++;      
-	        if(um[txt[j]]==0)
+	            
+	            if(um[txt[j]]==0)
 	            count--;
 	        }
-	    if(j-i+1<k) j++;
-	    
-	    else if(j-i+1==k){
-	        if(count==0)
+	        if(j-i+1<k) j++;
+	        
+	        else if(j-i+1==k){
+	            if(count==0)
 	            ans++;
-	        // check before sliding the window;
-	        if(um.find(txt[i])!=um.end()){
-	            um[txt[i]]++;
-	        if(um[txt[i]]==1)
-	        count++;
+	            
+	            if(um.find(txt[i])!=um.end()){
+	                um[txt[i]]++;
+	                
+	                if(um[txt[i]]==1) count++;
+	            }
+	        i++;
+	        j++;
+	        }
 	    }
-	    i++;
-	    j++;
-	    }
-	}
-	return ans;
+	    return ans;
 	}
 
 };
