8aa5c26653bd3d5e3f2948da7b550c23aace936b
jitender1222 <93830004+jitender1222@users.noreply.github.com>
2022-04-25 15:19:43 +0530

    Time: 203 ms (39.80%), Space: 75.2 MB (6.18%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int i=0,j=0;
+        map<int,int>mp;
+        int ans=0;
+        
+        if(fruits.size()==1){
+            return 1;
+        }
+        
+        while(j<fruits.size()){
+            
+            mp[fruits[j]]++;
+            
+            // if(mp.size()<2) j++;
+            
+             if(mp.size()<=2){
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            else if(mp.size()>2){
+                
+                while(mp.size()>2){
+                    mp[fruits[i]]--;
+                    
+                    if(mp[fruits[i]]==0){
+                        mp.erase(fruits[i]);
+                    }
+                    i++;
+                }
+                 j++;
+            }
+        }
+        return ans;
+    }
+};
